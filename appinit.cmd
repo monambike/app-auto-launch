2230e22aad1b2177514a7740ae2b2d60157e1750
Vinícius Gabriel Marques de Melo <vinicius_gabriel258@hotmail.com>
2020-08-15 18:09:39 -0300

    Change design and add disclaimer

--- appinit.cmd
+++ appinit.cmd
@@ -1,7 +1,12 @@
 @ECHO OFF
+COLOR 0D
 TITLE APPS INITIALIZER v0.2
 
 :optionsMenu
+ECHO APPS INITIALIZER
+ECHO.
+ECHO Please note that the files path may be wrong, be sure of edit the cmd file before use it.
+ECHO.
 ECHO +---------------------------------------------+
 ECHO \                                             /
 ECHO \  PLEASE CHOOSE THE APPS THAT U WANT TO OPEN /
@@ -17,8 +22,10 @@ ECHO \ B. Calculator                               /
 ECHO \                                             /
 ECHO +---------------------------------------------+
 
-ECHO Please, select an app typing the <letter> of the app that you want to open.
-CHOICE /C 0123AB /N /M "After you decide wich apps do you want to open, press 0 to open."
+ECHO "Please, select an app typing the <letter> of the app that you want to open." ^
+"After you decide wich apps do you want to open, press 0 to open everything."
+CHOICE /C 0123AB /N /M ""
+
 IF %ERRORLEVEL% EQU 0 GOTO :openApps
 IF %ERRORLEVEL% EQU 1 GOTO :funnyFunction
 
