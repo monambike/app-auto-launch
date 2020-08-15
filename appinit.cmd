6c76a21ccac23dc5828f54ecfa3ec24e078e0a7d
Vinícius Gabriel Marques de Melo <vinicius_gabriel258@hotmail.com>
2020-08-15 18:10:23 -0300

    Add comments and make code more readable

--- appinit.cmd
+++ appinit.cmd
@@ -1,7 +1,9 @@
 @ECHO OFF
+:: Personalization of CMD
 COLOR 0D
 TITLE APPS INITIALIZER v0.2
 
+:: Design of menu
 :optionsMenu
 ECHO APPS INITIALIZER
 ECHO.
@@ -22,6 +24,7 @@ ECHO \ B. Calculator                               /
 ECHO \                                             /
 ECHO +---------------------------------------------+
 
+:: Choice
 ECHO "Please, select an app typing the <letter> of the app that you want to open." ^
 "After you decide wich apps do you want to open, press 0 to open everything."
 CHOICE /C 0123AB /N /M ""
@@ -29,12 +32,14 @@ CHOICE /C 0123AB /N /M ""
 IF %ERRORLEVEL% EQU 0 GOTO :openApps
 IF %ERRORLEVEL% EQU 1 GOTO :funnyFunction
 
+:: Function to open app
 :openApps
 ECHO "U pressed 0 fool"
 PAUSE
 CLS
 GOTO :optionsMenu
 
+::Function for testing
 :funnyFunction
 ECHO "U pressed 1 fool"
 PAUSE
