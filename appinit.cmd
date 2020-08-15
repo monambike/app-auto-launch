b0956e81a99ff7f232af4a8f4b18d61d558da841
Vinícius Gabriel Marques de Melo <vinicius_gabriel258@hotmail.com>
2020-08-15 16:39:41 -0300

    Add interface of choice
    The interface of choice has been made.

--- appinit.cmd
+++ appinit.cmd
@@ -1,8 +1,20 @@
 @ECHO OFF
 TITLE APPS INITIALIZER v0.2
 
-:FUNC
-FOR /L %%x IN (1, 1, 100) DO CALL :F1
+ECHO +---------------------------------------------+
+ECHO \                                             /
+ECHO \  PLEASE CHOOSE THE APPS THAT U WANT TO OPEN /
+ECHO \                                             /
+ECHO \ 0. Open Chosen Apps                         /
+ECHO \ 1. Clear                                    /
+ECHO \ 2. Cancel                                   /
+ECHO \ 3. Exit                                     /
+ECHO \                                             /
+ECHO \ APPS:                                       /
+ECHO \ A. Sublime Text 3                           /
+ECHO \ B. Calculator                               /
+ECHO \                                             /
+ECHO +---------------------------------------------+
 
-:F1
-ECHO %%x
+CHOICE /C 0123AB /M "Please, select an app typing the <letter> of the app that you want to open. After you decide wich apps do you want to open, press 0 to open."
+CALL IF %ERRORLEVEL% EQU 0 PAUSE
