382a144092615cfe7672c1afb5b4ac35c0896847
Vinícius Gabriel Marques de Melo <vinicius_gabriel258@hotmail.com>
2020-08-17 15:37:39 -0300

    Change '::' for 'REM'
    I did this change because, sometimes, when debugging code, appeared an error related to find drivers, and I discovered that was because of '::'.

--- appinit.cmd
+++ appinit.cmd
@@ -1,13 +1,13 @@
 @ECHO OFF
 SETLOCAL enabledelayedexpansion
-:: Personalization of CMD
+REM Personalization of CMD
 COLOR 0D
 TITLE APPS INITIALIZER v0.2
 
-:: Variables
+REM Variables
 SET "userOption"
-:: The option letter is organized according it's position
-:: E.g. 0=A, 1=B
+REM The option letter is organized according it's position
+REM E.g. 0=A, 1=B
 SET app[0]=Sublime Text 3
 SET app[1]=Discord
 SET app[2]=XAMPP
@@ -15,7 +15,7 @@ SET app[3]=Git
 SET app[4]=CMD
 SET app[5]=Opera
 
-:: Design of menu
+REM Design of menu
 :optionsMenu
 ECHO +--------------------------------------------------+
 ECHO [               APP INITIALIZER                    ]
@@ -56,22 +56,23 @@ ECHO [ [                      ]                         ]
 ECHO [ ________________________                         ]
 ECHO +--------------------------------------------------+
 
-:: Choice
+REM Choice
 ECHO "Please, select an app typing the <letter> of the app that you want to open." ^
 "After you decide wich apps do you want to open, press 0 to open everything."
 ECHO.
 ECHO Your apps: '%userOption%'
 CHOICE /C 012ABCDEF /N /M "Choice: "
 
-:: COMMANDS
-:: If user has chosen to Open Apps
+REM COMMANDS
+REM If user has chosen to Open Apps
 IF %ERRORLEVEL% EQU 1 (
 	ECHO Open chosen apps
 	PAUSE
 	CLS
 	GOTO :optionsMenu
 )
-:: If user has chosen to Cancel Operation
+
+REM If user has chosen to Cancel Operation
 IF %ERRORLEVEL% EQU 2 (
 	REM ECHO Cancel
 	REM PAUSE
@@ -79,61 +80,61 @@ IF %ERRORLEVEL% EQU 2 (
 	REM GOTO :optionsMenu
 	GOTO :readArray
 )
-:: If user has chosen to Exit Program
+REM If user has chosen to Exit Program
 IF %ERRORLEVEL% EQU 3 (
 	ECHO Exit
 	PAUSE
 	CLS
 	GOTO :optionsMenu
 )
-:: SERVICES
-:: If user has chosen Sublime Text 3
+REM SERVICES
+REM If user has chosen Sublime Text 3
 IF %ERRORLEVEL% EQU 4 (
 	SET userOption=%userOption%A
 	CLS
 	GOTO :optionsMenu
 )
-:: If user has chosen Discord
+REM If user has chosen Discord
 IF %ERRORLEVEL% EQU 5 (
 	SET userOption=%userOption%B
 	CLS
 	GOTO :optionsMenu
 )
-:: If user has chosen XAMPP
+REM If user has chosen XAMPP
 IF %ERRORLEVEL% EQU 6 (
 	SET userOption=%userOption%C
 	CLS
 	GOTO :optionsMenu
 )
-:: If user has chosen Git
+REM If user has chosen Git
 IF %ERRORLEVEL% EQU 7 (
 	SET userOption=%userOption%D
 	CLS
 	GOTO :optionsMenu
 )
-:: If user has chosen CMD
+REM If user has chosen CMD
 IF %ERRORLEVEL% EQU 8 (
 	SET userOption=%userOption%E
 	CLS
 	GOTO :optionsMenu
 )
-:: If user has chosen Opera
+REM If user has chosen Opera
 IF %ERRORLEVEL% EQU 9 (
 	SET userOption=%userOption%F
 	CLS
 	GOTO :optionsMenu
 )
 
-:: Function to gather an exception
+REM Function to gather an exception
 ECHO "This value doesn't exist. Please, press any key and try again."
 GOTO :returnMenu
 
-:: Function to open app
+REM Function to open app
 :openApps
 ECHO "U pressed 0"
 GOTO :returnMenu
 
-::Function for testing
+REMFunction for testing
 :funnyFunction
 ECHO "You pressed 1"
 GOTO :returnMenu
@@ -144,8 +145,8 @@ PAUSE >NUL
 CLS
 GOTO :optionsMenu
 
-::Function for read array
+REMFunction for read array
 :readArray
-FOR /L %%i IN (0,1,5) DO ECHO In the position %%i , there's the app !app[%%i]!
+REMFOR /L %%i IN (0,1,5) DO ECHO In the position %%i , there's the app !app[%%i]!
 PAUSE >NUL
 ECHO PRESS ANY KEY TO CLOSE
